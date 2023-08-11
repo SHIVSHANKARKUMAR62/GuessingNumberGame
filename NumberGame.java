fdb066aaf5fcbd09bb2d44594f7ad4d58f081125
SHIVSHANKAR KUMAR <122685683+SHIVSHANKARKUMAR62@users.noreply.github.com>
2023-08-12 00:39:48 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 class Number{
 
-    int i, guess, n, k = 5, count = 0;
+    int i, guess,n,k=5,count =0;
 
     public void guessingNumber() {
         System.out.println("A number is chosen between 1 to 100. Guess the number within 5 trials.");
@@ -21,7 +21,7 @@ class Number{
 
 
                 if (number == guess) {
-                    System.out.println("Congratulations! You guessed the number.");
+                    System.out.println("Congratulations! You guessed the number in "+count+" chance.");
                     break;
                 } else if (number>guess) {
                     System.out.println("Guess Number is too Low...");

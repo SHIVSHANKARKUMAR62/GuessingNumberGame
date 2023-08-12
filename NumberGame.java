bb8f38b5e4160ffc64cb576a45ea1e13a09532f3
SHIVSHANKAR KUMAR <122685683+SHIVSHANKARKUMAR62@users.noreply.github.com>
2023-08-12 13:23:42 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -48,3 +48,53 @@ public class NumberGame {
         System.out.println("You take "+n.count+" chance...");
     }
 }
+package CodeSoftProject;
+
+import java.util.Scanner;
+
+class Number{
+
+    int i,guess,n,k = 5,count = 0;
+
+    public void guessingNumber() {
+        System.out.println("A number is chosen between 1 to 100 and Guess the number within 5 trials.");
+        do {
+            Scanner sc = new Scanner(System.in);
+
+            int number = 1 + (int) (100 * Math.random());
+
+            for (i = 0; i < k; i++) {
+
+                System.out.print("Guess the number: ");
+
+                guess = sc.nextInt();
+
+
+                if (number == guess) {
+                    System.out.println("Congratulations! You guessed the number in "+count+" chance.");
+                    break;
+                } else if (number>guess) {
+                    System.out.println("Guess Number is too Low...");
+                }else {
+                    System.out.println("Guess Number is too high...");
+                }
+                count++;
+            }
+            if (i == k) {
+                System.out.println("You have exhausted " + k + " trials.");
+                System.out.println("The number was " + number);
+                System.out.print("If You want to Get More Chance then Press 1 otherwise press any other Number key: ");
+                n = sc.nextInt();
+            }
+        }while (n==1);
+    }
+}
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Number n = new Number();
+        n.guessingNumber();
+        System.out.println("Thank You...");
+        System.out.println("You take "+n.count+" chance...");
+    }
+}

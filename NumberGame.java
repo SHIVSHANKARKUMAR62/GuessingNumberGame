c843a17ba25e356043e5ede3a220270eb88285a6
SHIVSHANKAR KUMAR <122685683+SHIVSHANKARKUMAR62@users.noreply.github.com>
2023-08-12 13:19:38 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -4,18 +4,21 @@ import java.util.Scanner;
 
 class Number{
 
-    int i, guess,n,k = 5,count = 0;
+    int i,guess,n,k = 5,count = 0;
 
     public void guessingNumber() {
-        System.out.println("A number is chosen between 1 to 100. Guess the number within 5 trials.");
+        System.out.println("A number is chosen between 1 to 100 and Guess the number within 5 trials.");
         do {
             Scanner sc = new Scanner(System.in);
 
             int number = 1 + (int) (100 * Math.random());
 
             for (i = 0; i < k; i++) {
+
                 System.out.print("Guess the number: ");
+
                 guess = sc.nextInt();
+
 
                 if (number == guess) {
                     System.out.println("Congratulations! You guessed the number in "+count+" chance.");
@@ -27,11 +30,10 @@ class Number{
                 }
                 count++;
             }
-            
             if (i == k) {
                 System.out.println("You have exhausted " + k + " trials.");
                 System.out.println("The number was " + number);
-                System.out.print("If You want to Get More Chance then Press 1 otherwise press any other number key: ");
+                System.out.print("If You want to Get More Chance then Press 1 otherwise press any other Number key: ");
                 n = sc.nextInt();
             }
         }while (n==1);
